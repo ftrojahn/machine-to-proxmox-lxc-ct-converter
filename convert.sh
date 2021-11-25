f184af2af26b8fc578e651685861e349ffef774d
Falko Trojahn <support@trojahn.de>
2021-11-25 21:37:30 +0100

    make tarcreate optional, use existing or default tar arch 'amd64' should be present in tar name

--- convert.sh
+++ convert.sh
@@ -4,6 +4,7 @@ usage()
     cat <<EOF
 $1 -h|--help
  -n|--name [lxc container name]
+ -c|--tarcreate [use target machine, if not set: standard debian 10]
  -t|--target [target machine ssh uri]
  -i|--id [proxmox container id]
  -s|--root-size [rootfs size in GB]
@@ -17,7 +18,7 @@ EOF
     return 0
 }
 
-options=$(getopt -o n:t:i:s:a:b:g:m:d:p:f -l help,name:,target:,id:,root-size:,ip:,bridge:,gateway:,memory:,disk-storage:,password:,foo: -- "$@")
+options=$(getopt -o n:c:t:i:s:a:b:g:m:d:p:f -l help,name:,tarcreate:,target:,id:,root-size:,ip:,bridge:,gateway:,memory:,disk-storage:,password:,foo: -- "$@")
 if [ $? -ne 0 ]; then
         usage $(basename $0)
     exit 1
@@ -29,10 +30,11 @@ do
     case "$1" in
         -h|--help)      usage $0 && exit 0;;
         -n|--name)      name=$2; shift 2;;
+        -c|--tarcreate) tarcreate=$2; shift 2;;
         -t|--target)    target=$2; shift 2;;
         -i|--id)        id=$2; shift 2;;
         -s|--root-size) rootsize=$2; shift 2;;
-        -a|--ip)       ip=$2; shift 2;;
+        -a|--ip)        ip=$2; shift 2;;
         -b|--bridge)    bridge=$2; shift 2;;
         -g|--gateway)   gateway=$2; shift 2;;
         -m|--memory)    memory=$2; shift 2;;
@@ -57,8 +59,21 @@ collectFS() {
 	.
 }
 
+if [ $tarcreate ] ; then
+# get from remote
 ssh "root@$target" "$(typeset -f collectFS); collectFS" \
-    > "/tmp/$name.tar.gz"
+    > "/tmp/${name}_amd64.tar.gz"
+template="/tmp/${name}_amd64.tar.gz"
+elif [ -f "/tmp/${name}_amd64.tar.gz" ] ; then
+# use existing
+template="/tmp/${name}_amd64.tar.gz"
+elif [ -f "/var/lib/vz/template/cache/${name}_amd64.tar.gz" ] ; then
+# use existing
+template="/var/lib/vz/template/cache/${name}_amd64.tar.gz"
+else
+# use default
+template="/var/lib/vz/template/cache/debian-10-standard_10.7-1_amd64.tar.gz"
+fi
 
 pct create $id "/tmp/$name.tar.gz" \
   --description LXC \
